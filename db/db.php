f269e5e9b1d1ea075317c9e00132755890517f2d
aayush <iushneupane08@gmail.com>
2025-12-06 14:06:55 +0545

    added the file now again

---
+++ db/db.php
@@ -0,0 +1,15 @@
+<?php
+<?php
+define('DB_HOST', '127.0.0.1');
+define('DB_USER', 'root');    // XAMPP default
+define('DB_PASS', '');        // XAMPP default
+define('DB_NAME', 'fixnepal');
+
+function db_connect() {
+  $m = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+  if ($m->connect_error) {
+    die('DB connect error');
+  }
+  return $m;
+}
+?>
